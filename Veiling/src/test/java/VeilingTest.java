006cf2406a7ee82bb24539752a7e3d523670b9b6
Sam Meulen <sam.meulen@vdabcampus.be>
2019-03-04 09:08:04 +0100

    Jenny

--- Veiling/src/test/java/VeilingTest.java
+++ Veiling/src/test/java/VeilingTest.java
@@ -1,22 +1,26 @@
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class VeilingTest {
+	private Veiling veiling;
+	@Before
+	public void before() {
+	veiling = new Veiling();
+	}
+	
 	@Test
 	public void hoogsteBodIsNul() {
-	Veiling veiling = new Veiling ();
-	assertEquals(0, new Veiling().getHoogsteBod());
+	assertEquals(0, veiling.getHoogsteBod());
 	}
 	@Test
 	public void hoogsteBodIsEersteBod() {
-	Veiling veiling = new Veiling ();
 	veiling.doeBod(100);
 	assertEquals(100, veiling.getHoogsteBod());
 	}
 	@Test
 	public void meerdereBiedingen() {
-	Veiling veiling = new Veiling ();
 	veiling.doeBod(200);
 	veiling.doeBod(100);
 	assertEquals(200, veiling.getHoogsteBod());
